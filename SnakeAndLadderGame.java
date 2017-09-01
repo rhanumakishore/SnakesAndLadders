25e0a317e7eaa52999ef76d609142f39d888c6fe
rhanumakishore <v-rhanumakishore@expedia.com>
2017-09-01 16:40:03 +0530

    class name updated

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
 
-public class SLTest {
+public class SnakeAndLadderGame {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
